d3e8b74517a37917054c9a9afdf67c20e0129a31
bensammwaniki <bensammwaniki@gmail.com>
2021-10-27 13:39:11 +0300

    added a signup page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,22 +1,39 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import { RouterModule, Routes } from '@angular/router';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MaincomponetsComponent } from './maincomponets/maincomponets.component';
 import { HomecomponetsComponent } from './homecomponets/homecomponets.component';
 import { SubjectComponent } from './subject/subject.component';
+import { LoginComponent } from './login/login.component';
+import { SignupComponent } from './signup/signup.component';
+
+
+const appRoutes: Routes = [
+  { path: '', component: HomecomponetsComponent },
+  { path: 'subject', component: SubjectComponent },
+  { path: 'login', component: LoginComponent },
+  { path: 'signup', component: SignupComponent },
+];
 
 @NgModule({
   declarations: [
     AppComponent,
     MaincomponetsComponent,
     HomecomponetsComponent,
-    SubjectComponent
+    SubjectComponent,
+    LoginComponent,
+    SignupComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule.forRoot(
+      appRoutes,
+      { enableTracing: true } // <-- debugging purposes only
+    )
   ],
   providers: [],
   bootstrap: [AppComponent]
